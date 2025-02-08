d4fae19f66392e34378cdb27ef206bf6eb7b25df
gurevapolina <gurevapolina98@gmail.com>
2025-02-08 13:03:28 +0000

    Add py script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+from time import sleep
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("ETH challange - 2D2CO2")
+
+st.subheader("Concept")
+st.text("Description of the project - TBA")
+
+uploadedFiles = st.file_uploader(
+    label="Upload your PDF file",
+    type=['pdf', 'dxf'],
+    accept_multiple_files=True,
+    label_visibility="hidden"
 )
+
+if len(uploadedFiles) != 0:
+    with st.spinner("Creating 3D plan..."):
+        sleep(10)
